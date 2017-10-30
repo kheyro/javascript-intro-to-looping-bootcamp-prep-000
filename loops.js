5e2a05f44eb0b4ec3d4a92e37f1ecfb2e50ba3f5
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 10:52:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,6 +11,12 @@ function whileLoop(n) {
   return "done";
 }
 
-function doWhileLoop(array) {
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 
+function doWhileLoop(array) {
+  do {
+    
+  } while (array.length > 0 && maybeTrue());
 }
